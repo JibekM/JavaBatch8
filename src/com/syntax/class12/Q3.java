c8f9ec16af1c3690e5da8c2072693cce4c054c68
JibekM <jibek.mambetkazieva@gmail.com>
2020-10-25 22:11:33 -0400

    Group Tasks

---
+++ src/com/syntax/class12/Q3.java
@@ -0,0 +1,26 @@
+package com.syntax.class12;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * Write a Java Program to print the first 10 numbers of Fibonacci series.
+		 */
+
+		int a = 0;
+		int b = 1;
+		int sum;
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(a+" ");
+			sum = a + b;
+			//System.out.print(sum+" ");
+
+			a = b;
+			b = sum;
+	}
+
+}
+}
